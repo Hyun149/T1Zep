0a62aa4aef311ca9a19c148919af336e11d9d88d
Hyun149 <rusia4527@Gmail.com>
2025-04-29 16:07:10 +0900

    feat: 플레이어 이동 기능 구현 및 Sprite 등록
    - Rigidbody2D 기반의 플레이어 이동 시스템 구현 (WASD / 화살표 키 대응)
    - SOLID 원칙에 따라 입력 처리 및 이동 처리를 구조적으로 분리
    - PlayerMovement.cs 추가 및 인스펙터용 속성은 SerializeField로 설정
    - Faker 스프라이트 이미지 및 메타 등록
    - SampleScene에 테스트용 캐릭터 배치 완료
    - 불필요한 로그 및 캐시 파일은 .gitignore 적용 예정

---
+++ T1Zep/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveInput.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+}
