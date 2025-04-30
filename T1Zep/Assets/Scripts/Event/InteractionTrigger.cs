e660e8a74da3bab98ed68c90328ef7a15aa3c5f1
Hyun149 <rusia4527@Gmail.com>
2025-04-30 10:50:18 +0900

    feat: 이벤트 존 진입 시 트리거 실행 기능 구현
    - InteractionTrigger.cs 추가
      - 플레이어가 Trigger Collider 진입 시 메시지 출력 및 이벤트 트리거
      - 향후 미니게임, 컷씬 연동 가능하도록 확장 구조 설계
    
    - EventZone.png 및 관련 메타 추가
      - 이벤트 구역 시각화용 이미지 리소스 반영
    
    - SampleScene 업데이트
      - 이벤트 존 배치 및 InteractionTrigger 적용 완료

---
+++ T1Zep/Assets/Scripts/Event/InteractionTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionTrigger : MonoBehaviour
+{
+    [SerializeField] private string message = "EVENT MAP: 이벤트가 시작됩니다!";
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log(message);
+            // TODO; 미니게임 or 컷씬 시작
+        }
+    }
+}
